639471a54c4b0e819f0b433da72160dc971a5570
David Oduneye <davidoduneye1@gmail.com>
2025-05-10 11:56:08 -0400

    tmux config upgrades

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,6 @@
 # ~/.config/fish/config.fish
 
-# Environment
+# environment
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx PAGER less
@@ -8,7 +8,7 @@ set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH /opt/homebrew/opt/postgresql@15/bin $PATH
 
-# Activate tools
+# activate tools
 mise activate fish | source
 zoxide init fish | source
 atuin init fish | source 
@@ -16,7 +16,7 @@ starship init fish | source
 fzf --fish | source
 thefuck --alias | source 
 
-# Aliases (fish shell uses 'alias' too)
+# aliases (fish shell uses 'alias' too)
 alias restart="source ~/.config/fish/config.fish"
 alias c="clear"
 alias cat="bat"
@@ -25,6 +25,7 @@ alias ls="eza -alh"
 alias cd="z"
 alias vi="nvim"
 
+# git aliases
 alias gm="git checkout main"
 alias gmp="git checkout main && git pull"
 alias gg="git log --online --graph --all"
@@ -38,9 +39,16 @@ alias gu="git reset --soft HEAD~1"
 abbr -a gcm "git commit -m"
 abbr -a gunstage "git reset HEAD --"
 
-# Disable greeting
+# tmux aliases
+alias t="tmux"
+alias ta="tmux attach -t"
+alias tls="tmux ls"
+alias tn="tmux new -s"
+alias tk="tmux kill-session -t"
+
+# disable greeting
 set fish_greeting ""
 
-# Added by OrbStack: command-line tools and integration
+# added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
